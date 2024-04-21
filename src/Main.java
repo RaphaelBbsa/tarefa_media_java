9a1e0f0029010577cd4877f42ce59901dea11d03
Raphael Barbosa <raphaelbarbosa779@gmail.com>
2024-04-21 09:54:30 -0300

    Subindo tarefa das médias

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Olá, vamos calcular sua média. ");
+        Notas notasProva = new Notas();
+        notasProva.setNota1(8);
+        notasProva.setNota2(7);
+        notasProva.setNota3(6);
+        notasProva.setNota4(3);
+
+        notasProva.exibeMedia();
+    }
+}
